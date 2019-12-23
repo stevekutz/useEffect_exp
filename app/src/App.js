6341ba1a963c750eced783d5c6707f66a5229a21
stevekutz <stkutz@gmail.com>
2019-12-22 21:59:25 -0600

    added simple list formatting with Images

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , {useEffect} from 'react';
+import {useState} from 'reinspect';
+import axios from 'axios';
+import {List, Image, Icon} from 'semantic-ui-react';
+import smiley from './img/happysmiley.jpg';
+import colorBurst from './img/ColourSurge1.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+const App = () => {
+
+    // the API requires data.hits
+
+    const [data, setFetchedData] = useState({ hits: [] });
+    useEffect(() => {
+      const fetched = async () => {
+        const fetchResult = await axios(
+          'https://hn.algolia.com/api/v1/search?query=redux',
+        );
+        setFetchedData(fetchResult.data);
+      };
+      fetched();
+    }, []);
+    return (
+        <div style = {{
+            backgroundImage: `url(${colorBurst})`,
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            position: 'absolute',
+            top: '0',
+            left: '0',
+            bottom: '0',
+            right: '0',
+            }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+            <List>
+                
+                {data.hits.map(item => (
+                    <List.Item key={item.objectID}>
+                        <Image src = {smiley}   size = 'mini' verticalAlign = 'middle' rounded spaced = 'left'/>
+                        <a style = {{color: 'lightgrey', padding: '2px'}} href={item.uList}>{item.title}</a>
+                    </List.Item>
+                ))}
+
+            </List>
+        </div>
+    );
+
+
 }
 
 export default App;
